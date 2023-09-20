d253fd67c0547b4c8922cac34be616060d8d75fe
Symon Ogwe <symonogwe@gmail.com>
2023-09-20 17:45:47 +0300

    Add close icon to close-drawer button

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,10 @@
 import menuSvg from "./Assets/menu.svg";
+import closeSvg from "./Assets/close.svg";
 
 const menuImg = document.querySelector(".menu-svg");
 menuImg.src = menuSvg;
+
+const cancelImg = document.querySelector(".cancel-img");
+cancelImg.src = closeSvg;
 
 export default menuImg;
