269dd38e3f6f54748894fe7bdba56a829ed90dfb
Symon Ogwe <symonogwe@gmail.com>
2023-09-20 21:38:47 +0300

    Implement drawer menu functionality

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,27 @@
 import menuSvg from "./Assets/menu.svg";
 import closeSvg from "./Assets/close.svg";
 
+// menu image src allocations
 const menuImg = document.querySelector(".menu-svg");
 menuImg.src = menuSvg;
 
 const cancelImg = document.querySelector(".cancel-img");
 cancelImg.src = closeSvg;
+
+// menu-btn functionality
+const revealMenuBtn = document.querySelector(".menu-btn");
+const drawerContainer = document.querySelector(".drawer-container");
+const hideDrawerBtn = document.querySelector(".close-drawer");
+
+function revealMenu() {
+  drawerContainer.classList.add("visible-drawer");
+}
+
+function hideDrawer() {
+  drawerContainer.classList.remove("visible-drawer");
+}
+
+revealMenuBtn.addEventListener("click", revealMenu);
+hideDrawerBtn.addEventListener("click", hideDrawer);
 
 export default menuImg;
